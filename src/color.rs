14631cf71bcdb1d8356d2bbb1df70a9cde1e8a23
Quentin Richert <quentin@richert.co>
2025-03-28 22:26:12 +0100

    tmp

--- src/color.rs
+++ src/color.rs
@@ -12,10 +12,7 @@ pub static NO_COLOR: LazyLock<bool> = LazyLock::new(|| {
     }
     // Contrary to `env::var()`, `env::var_os()` does not require the
     // value to be valid Unicode.
-    match env::var_os("NO_COLOR") {
-        Some(value) => !value.is_empty(),
-        None => false,
-    }
+    env::var_os("NO_COLOR").is_some_and(|v| !v.is_empty())
 });
 
 const ESC: &str = "\x1b[";
